cdcfc4ea4ab7a4e80924e6264d341b8e48c861ae
Jiovanny Andres Ibargüen <jiovanny007@gmail.com>
2016-10-13 18:50:52 -0500

    Initial Commit

---
+++ app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { HttpModule, JsonpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { MovieInfoComponent } from './movies/movie-info.component';
+
+@NgModule({
+  imports:      [ 
+  	BrowserModule,
+  	HttpModule,
+  	JsonpModule
+  ],
+  declarations: [ 
+  	AppComponent, 
+  	MovieInfoComponent 
+  ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
