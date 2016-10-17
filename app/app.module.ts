551eace8097d8b08c9ac9dd3873088690be65d81
Jiovanny Andres Ibargüen <jiovanny007@gmail.com>
2016-10-17 13:11:51 -0500

    Adding Routing Support
    Adding routing support for our appliacation

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { HttpModule, JsonpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { routing } from './app.routes';
 
 import { AppComponent } from './app.component';
 import { MovieInfoComponent } from './movies/components/movie-info.component';
@@ -10,7 +11,8 @@ import { MovieListComponent } from './movies/components/movie-list.component';
   imports:      [ 
   	BrowserModule,
   	HttpModule,
-  	JsonpModule
+  	JsonpModule,
+    routing
   ],
   declarations: [ 
   	AppComponent, 
