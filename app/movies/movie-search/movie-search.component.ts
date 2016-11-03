3faa91fdba4894e7ea3beead0ea4fcf7fea33aa2
Jiovanny Andres Ibargüen <jiovanny007@gmail.com>
2016-11-03 13:49:47 -0500

    Adding Movie's Credits
    Adding Movie's Credits

--- app/movies/movie-search/movie-search.component.ts
+++ app/movies/movie-search/movie-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit }   from '@angular/core';
+import { Component, HostListener }   from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Observable }  from 'rxjs/Observable'
 
@@ -11,7 +11,7 @@ import { MovieService } from '../shared/movie.service';
   styleUrls: ['app/movies/movie-search/movie-search.component.css']
 })
 export class MovieSearchComponent {
-  @Input() movies: Movie[];
+  movies: Movie[];
   name: string;
 
   // Constructor with injected service
@@ -26,7 +26,7 @@ export class MovieSearchComponent {
     this.getMoviesByName();
   }
 
-   // Get movies by name
+  // Get movies by name
   getMoviesByName() {
     this.movieService.getMoviesByName(this.name)
                       .subscribe(
@@ -37,4 +37,17 @@ export class MovieSearchComponent {
                         });
   }
 
+  // Pagination
+  /*@HostListener('window:scroll', ['$event'])
+    track(event) {
+      let windowHeight = "innerHeight" in window ? window.innerHeight : document.documentElement.offsetHeight;
+      let body = document.body, html = document.documentElement;
+      let docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight,  html.scrollHeight, html.offsetHeight);
+      let windowBottom = windowHeight + window.pageYOffset;
+      if (windowBottom >= docHeight) {
+        let nextPage = this.page++;
+        this.getMoviesByName(this.name, nextPage);
+        console.log(this.page);
+      }
+    }*/
 }
